b6a81721fe6d456ab8d147180c72683c1d2ed172
MauroGITGlobal <maurrx1@gmail.com>
2024-11-27 10:52:28 -0300

    correccion...

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from os import path
+from decouple import  config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres.fxuupwfmgicmtldsoknw',
+        'USER': 'postgres.ribitdwzpmxattgpnapf',
         'HOST': config('SUPABASE_HOST'),
         'PASSWORD': config('SUPABASE_PASSWORD'),
         'PORT': '6543',
